0610d832ab3201258b058d7754e5d2d4ca4f1fba
Noble Mathews <55821103+NobleMathews@users.noreply.github.com>
2021-01-21 23:38:18 +0530

    Removed Latest
    Page removed way too buggy and prabhat bhaiya updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,10 +17,6 @@ module.exports = {
         name: 'Home',
       },
       {
-        link: '/latest/',
-        name: 'Latest Issue',
-      },
-      {
         link: '/about/',
         name: 'Our Team',
       },
