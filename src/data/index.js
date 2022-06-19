d4f3685ecc8fa75b01227b567740de1e7f699077
sirish123 <cs20b043@iittp.ac.in>
2022-06-19 23:38:58 +0530

    minor fixes

--- src/data/index.js
+++ src/data/index.js
@@ -1,18 +1,18 @@
 // key identifies file name for image and is used in every href
-// value 
+// value
 
 export const newsletter = [
   {
     key: 'prabhat',
-    email:'literarysec@iittp.ac.in',
+    email: 'literarysec@iittp.ac.in',
     title: 'Prabhat Reddy',
-    link: 'mailto:literarysec@iittp.ac.in'
+    link: 'mailto:literarysec@iittp.ac.in',
   },
   {
     key: 'noble',
-    email:'ch19b023@iittp.ac.in',
+    email: 'ch19b023@iittp.ac.in',
     title: 'Noble Saji Mathews',
-    link: 'https://in.linkedin.com/in/noble-saji-mathews'
+    link: 'https://in.linkedin.com/in/noble-saji-mathews',
   },
   {
     key: 'shreyansh',
@@ -22,182 +22,195 @@ export const newsletter = [
   },
   {
     key: 'debeshee',
-    email:'cs19b012@iittp.ac.in',
+    email: 'cs19b012@iittp.ac.in',
     title: 'Debeshee Das',
-    link: 'https://www.linkedin.com/in/debeshee-das/'
+    link: 'https://www.linkedin.com/in/debeshee-das/',
   },
   {
     key: 'krithi',
-    email:'ee20b038@iittp.ac.in',
+    email: 'ee20b038@iittp.ac.in',
     title: 'Krithi Shailya',
-    link: 'https://www.linkedin.com/in/krithi-shailya-443758215'
+    link: 'https://www.linkedin.com/in/krithi-shailya-443758215',
   },
   {
     key: 'sneha',
-    email:'ch20b022@iittp.ac.in',
+    email: 'ch20b022@iittp.ac.in',
     title: 'Sneha M S',
-    link: 'mailto:ch20b022@iittp.ac.in'
+    link: 'https://www.linkedin.com/in/snems/',
   },
   {
     key: 'anant',
-    email:'ee20b003@iittp.ac.in',
+    email: 'ee20b003@iittp.ac.in',
     title: 'Anant Tyagi',
-    link: 'https://www.linkedin.com/in/anant-tyagi-80430a1ba'
+    link: 'https://www.linkedin.com/in/anant-tyagi-80430a1ba',
   },
   {
     key: 'sathyanarayann',
-    email:'ee20b046@iittp.ac.in',
+    email: 'ee20b046@iittp.ac.in',
     title: 'Sathyanarayann',
-    link: 'mailto:ee20b046@iittp.ac.in'
+    link: 'mailto:ee20b046@iittp.ac.in',
   },
   {
     key: 'battula',
-    email:'me21b045@iittp.ac.in',
+    email: 'me21b045@iittp.ac.in',
     title: 'Sneha Battula',
-    link: 'mailto:me21b045@iittp.ac.in'
+    link: 'mailto:me21b045@iittp.ac.in',
   },
   {
     key: 'preethi',
-    email:'cs20b030@iittp.ac.in',
+    email: 'cs20b030@iittp.ac.in',
     title: 'Preethi Varsha Marivina',
-    link: 'https://www.linkedin.com/in/preethi-varsha-70211b210'
+    link: 'https://www.linkedin.com/in/preethi-varsha-70211b210',
   },
   {
     key: 'kranthi',
-    email:'cs19b036@iittp.ac.in',
+    email: 'cs19b036@iittp.ac.in',
     title: 'Kranthi Sedamaki',
-    link: 'https://www.linkedin.com/in/ganesh-priyatham-palivela-abb599229'
+    link: 'https://www.linkedin.com/in/kranthisedamaki/',
   },
   {
     key: 'sirish',
-    email:'cs20b043@iittp.ac.in',
+    email: 'cs20b043@iittp.ac.in',
     title: 'Sirish',
-    link: 'https://www.linkedin.com/in/sirish-sekhar-6896b51a7/'
+    link: 'https://www.linkedin.com/in/sirish-sekhar-6896b51a7/',
   },
   {
     key: 'mohith',
-    email:'ch20b021@iittp.ac.in',
+    email: 'ch20b021@iittp.ac.in',
     title: 'Ram Mohith',
-    link: 'https://www.linkedin.com/in/ram-mohith-223386218'
+    link: 'https://www.linkedin.com/in/ram-mohith-223386218',
   },
   {
     key: 'swathi',
-    email:'ce20b010@iittp.ac.in',
+    email: 'ce20b010@iittp.ac.in',
     title: 'Swathi',
-    link: 'https://www.linkedin.com/in/swathi-gummadi-67826a226/'
+    link: 'https://www.linkedin.com/in/swathi-gummadi-67826a226/',
   },
   {
     key: 'sachin',
-    email:'cs20b034@iittp.ac.in',
+    email: 'cs20b034@iittp.ac.in',
     title: 'Sachin Kumar Sahu',
-    link: 'https://www.linkedin.com/in/sachin431/'
+    link: 'https://www.linkedin.com/in/sachin431/',
   },
   {
     key: 'mathew',
-    email:'cs20b021@iittp.ac.in',
+    email: 'cs20b021@iittp.ac.in',
     title: 'Mathew KJ',
-    link: 'https://www.linkedin.com/in/mathew-k-j-81957623b/'
+    link: 'https://www.linkedin.com/in/mathew-k-j-81957623b/',
   },
   {
     key: 'ganesh',
-    email:'cs20b026@iittp.ac.in',
+    email: 'cs20b026@iittp.ac.in',
     title: 'Ganesh Priyatham',
-    link: 'https://www.linkedin.com/in/ganesh-priyatham-palivela-abb599229'
+    link: 'https://www.linkedin.com/in/ganesh-priyatham-palivela-abb599229',
   },
   {
     key: 'saurabh',
-    email:'cs20b038@iittp.ac.in',
+    email: 'cs20b038@iittp.ac.in',
     title: 'Saurabh Kumar',
-    link: 'https://www.linkedin.com/in/ganesh-priyatham-palivela-abb599229'
-  }
-]
+    link: 'https://www.linkedin.com/in/saurabh-kumar-089ab4241',
+  },
+];
+export const pastmembers = [
+  {
+    key: 'prabhat',
+    email: 'literarysec@iittp.ac.in',
+    title: 'Prabhat Reddy',
+    link: 'mailto:literarysec@iittp.ac.in',
+  },
+  {
+    key: 'shreyansh',
+    email: 'cs18b042@iittp.ac.in',
+    title: 'Shreyansh Mehra',
+    link: 'https://www.linkedin.com/in/shreyansh-mehra/',
+  },
+];
 
 export const members = [
-    {
-      key: 'shreyansh',
-      pos: 'Literary Affairs Secretary',
-      email:'literarysec@iittp.ac.in',
-      title: 'Shreyansh Mehra',
-      link:'https://www.linkedin.com/in/shreyansh-mehra/' 
-    },
-    {
-      key: 'sudarshan',
-      pos: 'Debate Club Coordinator',
-      email:'ee18b034@iittp.ac.in',
-      title: 'Sudarshan H V',
-      link: 'https://www.linkedin.com/in/sudarshanhv'
-    },
-    {
-      key: 'madhavan',
-      pos: 'Quiz Club Coordinator',
-      email:'ch18b015@iittp.ac.in',
-      title: 'Madhavan Nampoothiri',
-      link: 'https://www.linkedin.com/in/madhavan-nampoothiri-d-k-80b96a199/'
-    },
-    {
-      key: 'noble',
-      pos: 'Student Newsletter Coordinator',
-      email:'ch19b023@iittp.ac.in',
-      title: 'Noble Saji Mathews',
-      link: 'https://in.linkedin.com/in/noble-saji-mathews'
-    },
-    {
-      key: 'keerthana',
-      pos: 'Student Newsletter Committee',
-      email:'ee19b035@iittp.ac.in',
-      title: 'Keerthana',
-      link: 'mailto:ee19b035@iittp.ac.in'
-    },
-    {
-      key: 'utkarsh',
-      pos: 'Member',
-      email:'ee19b042@iittp.ac.in',
-      title: 'Utkarsh Pandya',
-      link: 'mailto:ee19b042@iittp.ac.in'
-    },
-    {
-      key: 'aasim',
-      pos: 'Member',
-      email:'ch18b017@iittp.ac.in',
-      title: 'Mohd Aasim Khan',
-      link: 'https://www.linkedin.com/in/mohd-aasim-khan-5a5a5b134/'
-    },
-    {
-      key: 'prasoon',
-      pos: 'Member',
-      email:'ch18b021@iittp.ac.in',
-      title: 'Prasoon Gupta',
-      link: 'https://www.linkedin.com/in/prasoon-gupta-5b544b1b4'
-    },
-    {
-      key: 'abhay',
-      pos: 'Member',
-      email:'ch18b001@iittp.ac.in',
-      title: 'Abhay Singh',
-      link: 'www.linkedin.com/in/abhaysingh2001'
-    },
-    {
-      key: 'pranav',
-      pos: 'Member',
-      email:'ee19b037@iittp.ac.in',
-      title: 'Pranav Hari Panicker',
-      link: 'mailto:ee19b037@iittp.ac.in'
-    }
-  
-  ]
+  {
+    key: 'shreyansh',
+    pos: 'Literary Affairs Secretary',
+    email: 'literarysec@iittp.ac.in',
+    title: 'Shreyansh Mehra',
+    link: 'https://www.linkedin.com/in/shreyansh-mehra/',
+  },
+  {
+    key: 'sudarshan',
+    pos: 'Debate Club Coordinator',
+    email: 'ee18b034@iittp.ac.in',
+    title: 'Sudarshan H V',
+    link: 'https://www.linkedin.com/in/sudarshanhv',
+  },
+  {
+    key: 'madhavan',
+    pos: 'Quiz Club Coordinator',
+    email: 'ch18b015@iittp.ac.in',
+    title: 'Madhavan Nampoothiri',
+    link: 'https://www.linkedin.com/in/madhavan-nampoothiri-d-k-80b96a199/',
+  },
+  {
+    key: 'noble',
+    pos: 'Student Newsletter Coordinator',
+    email: 'ch19b023@iittp.ac.in',
+    title: 'Noble Saji Mathews',
+    link: 'https://in.linkedin.com/in/noble-saji-mathews',
+  },
+  {
+    key: 'keerthana',
+    pos: 'Student Newsletter Committee',
+    email: 'ee19b035@iittp.ac.in',
+    title: 'Keerthana',
+    link: 'mailto:ee19b035@iittp.ac.in',
+  },
+  {
+    key: 'utkarsh',
+    pos: 'Member',
+    email: 'ee19b042@iittp.ac.in',
+    title: 'Utkarsh Pandya',
+    link: 'mailto:ee19b042@iittp.ac.in',
+  },
+  {
+    key: 'aasim',
+    pos: 'Member',
+    email: 'ch18b017@iittp.ac.in',
+    title: 'Mohd Aasim Khan',
+    link: 'https://www.linkedin.com/in/mohd-aasim-khan-5a5a5b134/',
+  },
+  {
+    key: 'prasoon',
+    pos: 'Member',
+    email: 'ch18b021@iittp.ac.in',
+    title: 'Prasoon Gupta',
+    link: 'https://www.linkedin.com/in/prasoon-gupta-5b544b1b4',
+  },
+  {
+    key: 'abhay',
+    pos: 'Member',
+    email: 'ch18b001@iittp.ac.in',
+    title: 'Abhay Singh',
+    link: 'www.linkedin.com/in/abhaysingh2001',
+  },
+  {
+    key: 'pranav',
+    pos: 'Member',
+    email: 'ee19b037@iittp.ac.in',
+    title: 'Pranav Hari Panicker',
+    link: 'mailto:ee19b037@iittp.ac.in',
+  },
+];
 
-  // ,
-  //   {
-  //     key: 'mannepalli',
-  //     pos: 'Member',
-  //     email:'cs18b036@iittp.ac.in',
-  //     title: 'Sripranav Mannepalli',
-  //     link: ''
-  //   },
-  //   {
-  //     key: 'spandana',
-  //     pos: 'Member',
-  //     email:'ce17b021@iittp.ac.in',
-  //     title: 'Sanagula Spandana',
-  //     link: ''
-  //   },
+// ,
+//   {
+//     key: 'mannepalli',
+//     pos: 'Member',
+//     email:'cs18b036@iittp.ac.in',
+//     title: 'Sripranav Mannepalli',
+//     link: ''
+//   },
+//   {
+//     key: 'spandana',
+//     pos: 'Member',
+//     email:'ce17b021@iittp.ac.in',
+//     title: 'Sanagula Spandana',
+//     link: ''
+//   },
