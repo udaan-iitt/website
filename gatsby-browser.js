c03cfb87b291fef14bf0b89ab1c2e10b2e1c76a5
Noble Mathews <55821103+NobleMathews@users.noreply.github.com>
2022-06-28 06:58:33 -0400

    Auto reload

---
+++ gatsby-browser.js
@@ -0,0 +1,17 @@
+// trigger an immediate page refresh when an update is found
+// export const onServiceWorkerUpdateReady = () => window.location.reload();
+
+export const onServiceWorkerUpdateReady = () => {
+    const answer = window.confirm(
+      `UDAAN has had a new update!` +
+        `Reload to display the latest version?`
+    )
+  
+    if (answer === true)  window.location.reload()
+}
+
+export const onRouteUpdate = () => {
+    navigator.serviceWorker.register('/sw.js').then((reg) => {
+        reg.update();
+    });
+};
