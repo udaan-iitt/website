21748e07ab07973c1d488010f4d098e40d95f202
Noble Mathews <55821103+NobleMathews@users.noreply.github.com>
2023-07-19 22:28:15 +0530

    feat : update website for release

--- src/pages/about.js
+++ src/pages/about.js
@@ -11,6 +11,8 @@ import "slick-carousel/slick/slick.css"
 import "slick-carousel/slick/slick-theme.css"
 import Slider from "react-slick"
 import Card from "Components/Card"
+import '@fontsource-variable/lora';
+
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import {
@@ -62,10 +64,13 @@ const About = () => {
     <Layout>
       <FadeIn>
         <Container rhythm={rhythm}>
-          <h1 style={{ textAlign: "center" }}>
-            <b>About UDAAN</b>
+          <h1 style={{ textAlign: "center", fontFamily:"'Lora Variable', sans-serif", marginBottom:"0px" }}>
+          About Udaan
           </h1>
-          <h4
+          <p style={{ textAlign: "center"}}>
+          Creativity. Expression. Inspiration.
+          </p>
+          {/* <h4
             style={{
               textAlign: "center",
               paddingTop: "10px",
@@ -75,56 +80,31 @@ const About = () => {
           >
             The first student-run campus newsletter of Indian Institute of
             Technology, Tirupati
-          </h4>
-          We initially envisioned Udaan as a record of all student activities in
-          the institute, but it turned out to be much more than that. Active
-          participation from many talented people helped us gather a wide
-          variety of content. The poems, articles, stories, reviews we have
-          received for Udaan in its first edition shows us its potential to
-          become the epicenter for student-made literary work. But a closer look
-          at this content tells us something more. There were original thoughts,
-          opinions, and most importantly, questions, concealed within words in
-          plain sight, a brief exposure to what Udaan can grow to be.
-          <br />
-          <br />
-          <b>Our Vision: </b>
-          <ul>
-            <li>
-              To evolve as a record of student experiences and activities within
-              our institute.
-            </li>
-            <li>
-              To encourage writing and become the center for all kinds of
-              student-made literary work.
-            </li>
-            <li>
-              To become a platform that enables students to constructively
-              discuss and come up with optimal solutions to various problems
-              within our institute.
-            </li>
-          </ul>
-          You may submit your entries for the next edition of the newsletter by
-          clicking on the <b>"Get Featured !"</b> link at the top right of the
-          page.
-          <br />
-          <br />
-          <b>
-            If you have any comments, suggestions, improvements, ideas for
-            different kinds of content or any other sort of feedback, please
-            feel free to share them with us and help us improve the newsletter!
-          </b>
-          <br />
-          <br />
+          </h4> */}
+          Udaan is not just a magazine; it is a vibrant hub of creativity and intellectual discourse within the IIT Tirupati community. Through its pages, we try to foster a sense of unity and belonging among the community, encouraging them to explore their talents and share their unique voices. With each issue, we strive to push the boundaries of conventional thinking, pushing readers to contemplate and engage with diverse ideas.
+          <br/><br/>
+          Our magazine serves as a catalyst for meaningful conversations, proving itself to be a platform for the IITT community to express their opinions on a wide range of topics, from social issues and scientific advancements to cultural events and campus initiatives. By delving into thought-provoking subjects, we aim to spark critical thinking and inspire our readers to examine the world around them from fresh perspectives.
+          <br/><br/>
+          Beyond our literary contributions, we also play a crucial role in documenting the milestones and achievements of the student community at IIT Tirupati. We capture the essence of campus life, showcasing the triumphs, challenges, and collective spirit that define the student experience. We strive to ensure that no story goes untold and that the rich tapestry of campus life is preserved for future generations. 
+          <br/><br/>
+          We thrive on the spirit of perpetual learning and discovery. With each edition, we eagerly embrace new initiatives and expand our horizons. Our passion for experimentation and growth led to the inception of Udaan Unthethered in July 2022. By incorporating audio as a medium, our magazine opens doors to even more diverse forms of creativity and unique voices.
+          <br/><br/>
+          Not just this, we are evolving and are coming up with ideas and articles not solely centred around IITT.  Tea-time Chat with the Author is the first of many such open-ended initiatives.
+          <br/><br/>
+          Recognizing the importance of innovation and adapting to evolving technologies, we have gone beyond the conventional magazine format. We have embraced the digital era by establishing an online presence and managing the web stack ourselves. In an effort to enhance the reader's experience, we introduced the 'audiobook experience' to our platform, bringing literature to life through spoken word.
+          <br/><br/>
+          We greatly value your input in our continuous pursuit of exploration and embracing novel experiences. We welcome any comments, suggestions, improvements, diverse content ideas, or any other form of feedback that could contribute to enhancing our magazine. Please don't hesitate to share your thoughts and help us elevate our literary adobe. 
+          <br/><br/>  
           <a
             target="_blank"
-            href="https://docs.google.com/forms/d/e/1FAIpQLSfJPjmIgl1PFEjr1GQI5IVlg76W83Hn79Xmyo6LeiQ4Zt7jDw/viewform?usp=sf_link"
+            href="https://forms.gle/P5sPZVwdmmJuU5mCA	"
           >
             Link to feedback form
           </a>
           <br />
-          <form action="https://forms.gle/SxQW7d9JikxRsV1s8" target="_blank">
+          {/* <form action="https://forms.gle/SxQW7d9JikxRsV1s8" target="_blank">
             <Button>JOIN US !</Button>
-          </form>
+          </form> */}
           <h2>Team UDAAN</h2>
           <hr />
           <>
--- src/pages/index.js
+++ src/pages/index.js
@@ -166,6 +166,21 @@ const Home = ({ pageContext, data }) => {
       <Main>
         <Content>
           <EditionFilter editionList={alleditions} categoryList={data.allMarkdownRemark.group} currented={currentEdition}/>
+          { currentEdition == "2023_Summer"&&
+            <Collapsible trigger="Editor's Note">
+              <p>In this delayed summer edition, as we toil from our abodes amidst the warmth of our families, we unveil a captivating array of tales that delve into the realms of love and kinship. Both stories, resilient in nature, cast a spotlight on profound themes. One unearths the horrors of colonisation and the underlying coloniality, underscoring the significance of safeguarding one's heritage and pursuing justice. The other serves as a testament to love's fortitude, triumphing over adversity and reminding us of the unwavering importance of championing righteousness amidst corruption and cruelty.</p>
+
+              <p>Within our poetic realm, Ariadne graciously bestows upon us the crimson threads that guide us through the labyrinthine depths of her mind - an intricate tapestry of unspoken desires. Ah, who among us does not possess such bittersweet memories? Furthermore, within our Op-Ed section, an extraordinary article elucidates the conjunctions and disjunctions of safety, while another seeks to decipher the cryptic musings of the enigmatic scriptwriter, chatGPT.</p>
+
+              <p>Our literary ensemble proudly presents two compelling reviews. The first beckons you to embark on a journey that promises transformation, though one that may lead to unexpected desolation. The second invites you to grapple with the intricate shades of good and evil in a tangible realm populated by characters lost in the vast expanse of a galaxy far, far away.</p>
+
+              <p>In the realm of campus news, we proudly present two cherished compilations. The first is our customary endeavour - &quot;Outlets of Expression&rdquo;, a report on the activities of our campus clubs. The second pays homage to our precursors in the online magazine domain&mdash;the revered newspaper where our team undertakes the whimsical task of offering an unfiltered yet affectionate portrayal of the academic year 22-23, aptly titled &quot;Our Take.&quot;</p>
+
+              <p>And lo and behold, as promised, we present another enthralling edition of &quot;Puppet Show.&quot; Within its enchanting embrace lies a captivating tribute, exquisitely capturing the untold narratives of the wondrous student body of 2022-23.</p>
+
+              <p>Embrace this journey, dear readers, and immerse yourselves in the wondrous realm of our Summer Edition.</p>
+            </Collapsible>
+          }
           { currentEdition == "2023_Spring"&&
             <Collapsible trigger="Editor's Note">
               <p>We like to think of this edition as a stroll through a luscious green park where the birds chirp, hailing the arrival of spring and flowers bloom, chasing away the winter gloom. In our first-ever Spring edition, we present &ldquo;Battling the Blues&rdquo;, easing into Chapter 1 of our mental health series &ldquo;It&rsquo;s the Climb&rdquo;. We also bring you a gentle stroll through academic affairs at IIT-T, consolidating a variety of surveys and inputs from students and faculty - like an exploratory morning walk where you&rsquo;re sure to discover some very interesting sights!</p>
