9a0a376f945e04f0c0fa94e255ef5a3aa0154afe
Sachin Sahu <75629410+SachinSahu431@users.noreply.github.com>
2023-10-02 07:37:53 +0000

    feat: add team pic

--- src/pages/about.js
+++ src/pages/about.js
@@ -11,8 +11,12 @@ import "slick-carousel/slick/slick.css"
 import "slick-carousel/slick/slick-theme.css"
 import Slider from "react-slick"
 import Card from "Components/Card"
-import '@fontsource-variable/lora';
+import "@fontsource-variable/lora"
 
+import { Carousel } from "react-responsive-carousel"
+import "react-responsive-carousel/lib/styles/carousel.min.css"
+import photo1 from "../images/groupPhoto/1.jpg"
+import photo2 from "../images/groupPhoto/2.jpg"
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import {
@@ -64,48 +68,87 @@ const About = () => {
     <Layout>
       <FadeIn>
         <Container rhythm={rhythm}>
-          <h1 style={{ textAlign: "center", fontFamily:"'Lora Variable', sans-serif", marginBottom:"0px" }}>
-          About Udaan
-          </h1>
-          <p style={{ textAlign: "center"}}>
-          Creativity. Expression. Inspiration.
-          </p>
-          {/* <h4
+          <h1
             style={{
               textAlign: "center",
-              paddingTop: "10px",
-              paddingBottom: "10px",
-              opacity: "0.6",
+              fontFamily: "'Lora Variable', sans-serif",
+              marginBottom: "0px",
             }}
           >
-            The first student-run campus newsletter of Indian Institute of
-            Technology, Tirupati
-          </h4> */}
-          Udaan is not just a magazine; it is a vibrant hub of creativity and intellectual discourse within the IIT Tirupati community. Through its pages, we try to foster a sense of unity and belonging among the community, encouraging them to explore their talents and share their unique voices. With each issue, we strive to push the boundaries of conventional thinking, pushing readers to contemplate and engage with diverse ideas.
-          <br/><br/>
-          Our magazine serves as a catalyst for meaningful conversations, proving itself to be a platform for the IITT community to express their opinions on a wide range of topics, from social issues and scientific advancements to cultural events and campus initiatives. By delving into thought-provoking subjects, we aim to spark critical thinking and inspire our readers to examine the world around them from fresh perspectives.
-          <br/><br/>
-          Beyond our literary contributions, we also play a crucial role in documenting the milestones and achievements of the student community at IIT Tirupati. We capture the essence of campus life, showcasing the triumphs, challenges, and collective spirit that define the student experience. We strive to ensure that no story goes untold and that the rich tapestry of campus life is preserved for future generations. 
-          <br/><br/>
-          We thrive on the spirit of perpetual learning and discovery. With each edition, we eagerly embrace new initiatives and expand our horizons. Our passion for experimentation and growth led to the inception of Udaan Unthethered in July 2022. By incorporating audio as a medium, our magazine opens doors to even more diverse forms of creativity and unique voices.
-          <br/><br/>
-          Not just this, we are evolving and are coming up with ideas and articles not solely centred around IITT.  Tea-time Chat with the Author is the first of many such open-ended initiatives.
-          <br/><br/>
-          Recognizing the importance of innovation and adapting to evolving technologies, we have gone beyond the conventional magazine format. We have embraced the digital era by establishing an online presence and managing the web stack ourselves. In an effort to enhance the reader's experience, we introduced the 'audiobook experience' to our platform, bringing literature to life through spoken word.
-          <br/><br/>
-          We greatly value your input in our continuous pursuit of exploration and embracing novel experiences. We welcome any comments, suggestions, improvements, diverse content ideas, or any other form of feedback that could contribute to enhancing our magazine. Please don't hesitate to share your thoughts and help us elevate our literary adobe. 
-          <br/><br/>  
-          <a
-            target="_blank"
-            href="https://forms.gle/P5sPZVwdmmJuU5mCA	"
-          >
+            About Udaan
+          </h1>
+          <p style={{ textAlign: "center" }}>
+            Creativity. Expression. Inspiration.
+          </p>
+          Udaan is not just a magazine; it is a vibrant hub of creativity and
+          intellectual discourse within the IIT Tirupati community. Through its
+          pages, we try to foster a sense of unity and belonging among the
+          community, encouraging them to explore their talents and share their
+          unique voices. With each issue, we strive to push the boundaries of
+          conventional thinking, pushing readers to contemplate and engage with
+          diverse ideas.
+          <br />
+          <br />
+          Our magazine serves as a catalyst for meaningful conversations,
+          proving itself to be a platform for the IITT community to express
+          their opinions on a wide range of topics, from social issues and
+          scientific advancements to cultural events and campus initiatives. By
+          delving into thought-provoking subjects, we aim to spark critical
+          thinking and inspire our readers to examine the world around them from
+          fresh perspectives.
+          <br />
+          <br />
+          Beyond our literary contributions, we also play a crucial role in
+          documenting the milestones and achievements of the student community
+          at IIT Tirupati. We capture the essence of campus life, showcasing the
+          triumphs, challenges, and collective spirit that define the student
+          experience. We strive to ensure that no story goes untold and that the
+          rich tapestry of campus life is preserved for future generations.
+          <br />
+          <br />
+          We thrive on the spirit of perpetual learning and discovery. With each
+          edition, we eagerly embrace new initiatives and expand our horizons.
+          Our passion for experimentation and growth led to the inception of
+          Udaan Unthethered in July 2022. By incorporating audio as a medium,
+          our magazine opens doors to even more diverse forms of creativity and
+          unique voices.
+          <br />
+          <br />
+          Not just this, we are evolving and are coming up with ideas and
+          articles not solely centred around IITT. Tea-time Chat with the Author
+          is the first of many such open-ended initiatives.
+          <br />
+          <br />
+          Recognizing the importance of innovation and adapting to evolving
+          technologies, we have gone beyond the conventional magazine format. We
+          have embraced the digital era by establishing an online presence and
+          managing the web stack ourselves. In an effort to enhance the reader's
+          experience, we introduced the 'audiobook experience' to our platform,
+          bringing literature to life through spoken word.
+          <br />
+          <br />
+          We greatly value your input in our continuous pursuit of exploration
+          and embracing novel experiences. We welcome any comments, suggestions,
+          improvements, diverse content ideas, or any other form of feedback
+          that could contribute to enhancing our magazine. Please don't hesitate
+          to share your thoughts and help us elevate our literary adobe.
+          <br />
+          <br />
+          <a target="_blank" href="https://forms.gle/P5sPZVwdmmJuU5mCA	">
             Link to feedback form
           </a>
           <br />
-          {/* <form action="https://forms.gle/SxQW7d9JikxRsV1s8" target="_blank">
-            <Button>JOIN US !</Button>
-          </form> */}
           <h2>Team UDAAN</h2>
+          <hr />
+          <Carousel autoplay infiniteLoop autoFocus emulateTouch>
+            <div>
+              <img src={photo1} alt="Slide 1" />
+            </div>
+            <div>
+              <img src={photo2} alt="Slide 2" />
+            </div>
+          </Carousel>
+          <h2>The Team</h2>
           <hr />
           <>
             <div className="img-grid lab-member">
@@ -180,38 +223,6 @@ const About = () => {
               })}
             </Slider>
           </div>
-          {/* <h2>Literary Affairs Council 2020</h2>
-      <hr/>
-      <>
-        <div className="img-grid lab-member">
-        {members.map((member) => (
-          <figure key={Math.random()} className="figure">
-            <a
-              href={`${member.link}`}
-              target="_blank"
-              referrerPolicy="no-referrer"
-            >
-            <div className="img-wrap">
-              <Image
-                filename={member.key}
-                />
-            </div>
-            </a>
-            <figcaption style={{padding:"0px",textAlign:"center",paddingTop:"10px"}} className="figure-caption text-center">
-              <a
-                href={`mailto:${member.email}`}
-                target="_blank"
-                referrerPolicy="no-referrer"
-              >
-              <b>{member.title}</b>
-              </a>
-              <br/>
-              <p style={{textAlign:"center",fontWeight:"bold",fontSize:"small!important",margin:"0px"}}>{member.pos}</p>
-            </figcaption>
-          </figure>
-        ))}
-        </div>
-      </> */}
         </Container>
       </FadeIn>
     </Layout>
